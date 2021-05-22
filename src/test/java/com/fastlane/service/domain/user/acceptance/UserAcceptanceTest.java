7d07237a338819d385270f32350cce6697609432
bgpark <bgpark82@gmail.com>
2021-05-22 22:50:13 +0900

    refactoring: 사용자 테스트 스텝 분리

--- src/test/java/com/fastlane/service/domain/user/acceptance/UserAcceptanceTest.java
+++ src/test/java/com/fastlane/service/domain/user/acceptance/UserAcceptanceTest.java
@@ -1,19 +1,16 @@
 package com.fastlane.service.domain.user.acceptance;
 
 import com.fastlane.service.domain.user.dto.UserRequest;
-import com.fastlane.service.domain.user.dto.UserRequestStub;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+
+import static com.fastlane.service.domain.user.step.UserStep.*;
 
 @DisplayName("사용자 관련 인수 테스트")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -31,17 +28,12 @@ public class UserAcceptanceTest {
     @Test
     void save() {
         // given
-        UserRequest userRequestStub = UserRequestStub.of("아이디", "비밀번호");
+        UserRequest userRequestStub = 사용자_생성_스텁();
 
         // when
-        ExtractableResponse<Response> response = RestAssured
-                .given().log().all()
-                .body(userRequestStub)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/api/v1/users")
-                .then().log().all().extract();
+        ExtractableResponse<Response> response = 사용자_생성_요청(userRequestStub);
 
         // then
-        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        사용자_생성_됨(response);
     }
 }
---
+++ src/test/java/com/fastlane/service/domain/user/step/UserStep.java
@@ -0,0 +1,30 @@
+package com.fastlane.service.domain.user.step;
+
+import com.fastlane.service.domain.user.dto.UserRequest;
+import com.fastlane.service.domain.user.dto.UserRequestStub;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+public class UserStep {
+
+    public static void 사용자_생성_됨(ExtractableResponse<Response> response) {
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    public static ExtractableResponse<Response> 사용자_생성_요청(UserRequest userRequestStub) {
+        return RestAssured
+                .given().log().all()
+                .body(userRequestStub)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/api/v1/users")
+                .then().log().all().extract();
+    }
+
+    public static UserRequest 사용자_생성_스텁() {
+        return UserRequestStub.of("아이디", "비밀번호");
+    }
+}
